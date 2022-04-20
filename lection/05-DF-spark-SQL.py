40866297b2f53c547e846999cc0135dacc571b33
VOpolskiy <vadopolski@gmail.com>
2022-04-20 23:41:08 +0200

    Added SQL working with metastore

--- lection/05-DF-spark-SQL.py
+++ lection/05-DF-spark-SQL.py
@@ -9,7 +9,58 @@ spark = SparkSession. \
     config("spark.sql.legacy.timeParserPolicy", "LEGACY"). \
     getOrCreate()
 
-cars_df = spark.read.json("../data/cars")
+def dml_ddl_expressions():
+    cars_df = spark.read.json("../sources/cars")
+
+    american_cars_df = cars_df.filter(col("Origin") == "Japan").select(col("Name"))
+    print("REGULAR DF API")
+    american_cars_df.show()
+
+    # store as a Spark table dont write data to storage EXTERNAL TABLE
+    #  DataFrame => SQL metastore
+    cars_df.createOrReplaceTempView("cars")
+    
+    # run SQL queries on top of DFs known to Spark under a certain name
+    print("SQL metastore => DataFrame")
+    american_cars_df_v2 = spark.sql("SELECT Name FROM cars WHERE Origin = 'Japan'")
+    american_cars_df_v2.show()
+
+    # DROPPING EXTERNAL TABLE DOES NOT DELETE DATA, ONLY IN METASTORE
+    print("DROPPING EXTERNAL TABLE")
+    spark.sql("DROP TABLE cars")
+    # spark.sql("SELECT Name FROM cars WHERE Origin = 'Japan'")
+
+    # store DFs as Spark tables (files known to Spark) to Spark storage HDFS
+    #  DataFrame => SQL metastore + Spark storage
+    print("SAVING TO MANAGED TABLE")
+    cars_df.write.mode("overwrite").saveAsTable("cars_managed_table")
+
+    print("READ FROM MANAGED STORAGE 1")
+    american_cars_df_v2 = spark.sql("SELECT * FROM cars_managed_table")
+    american_cars_df_v2.show()
+
+    print("READ FROM MANAGED STORAGE 2")
+    cars_managed_df = spark.table("cars_managed_table")
+    cars_managed_df.show()
+
+    print("DROPPING MANGED TABLE")
+    spark.sql("DROP TABLE cars_managed_table")
+
+    print("DDL DML INSIDE TABLE")
+    spark.sql("""CREATE SCHEMA test""")
+
+    spark.sql("CREATE TABLE test.students (name VARCHAR(64), address VARCHAR(64)) USING PARQUET PARTITIONED BY (student_id INT)")
+
+    spark.sql("INSERT INTO test.students VALUES('Bob Brown', '456 Taylor St, Cupertino', 222222),('Cathy Johnson', '789 Race Ave, Palo Alto', 333333)")
+
+    spark.sql("SELECT * FROM test.students").show()
+
+    print("DROPPING MANGED TABLE")
+    spark.sql("DROP TABLE test.students")
+
+     # https://spark.apache.org/docs/latest/sql-ref-syntax-dml-insert-into.html
+
+
 
 driver = "org.postgresql.Driver"
 url = "jdbc:postgresql://localhost:5432/rtjvm"
@@ -27,40 +78,25 @@ def read_table(table_name):
         option("dbtable", "public." + table_name). \
         load()
 
-
-employees_df = read_table("employees")
-salaries_df = read_table("salaries")
-dept_managers_df = read_table("dept_manager")
-dept_emp_df = read_table("dept_emp")
-departments_df = read_table("departments")
-
-# save table names
-employees_df.createOrReplaceTempView("employees")
-salaries_df.createOrReplaceTempView("salaries")
-dept_managers_df.createOrReplaceTempView("dept_manager")
-dept_emp_df.createOrReplaceTempView("dept_emp")
-departments_df.createOrReplaceTempView("departments")
-
-
-def dml_ddl_expressions():
-    # store as a Spark table
-    cars_df.createOrReplaceTempView("cars")
-
-    # select American cars
-    # regular DF API
-    american_cars_df = cars_df.filter(col("Origin") == "USA").select(col("Name"))
-    # run SQL queries on top of DFs known to Spark under a certain name
-    american_cars_df_v2 = spark.sql("select Name from cars where Origin = 'USA'")  # returns a DF
-
-    # store DFs as Spark tables (files known to Spark)
-    # cars_df.write.mode("overwrite").saveAsTable("cars")
-
-
 """
 1. show all employees and their max salary over time
 2. show all employees who were never managers
 3. for every employee, find the difference between their salary (current/latest) and the max salary of their department (departments table)
 """
+
+def exersices():
+    employees_df = read_table("employees")
+    salaries_df = read_table("salaries")
+    dept_managers_df = read_table("dept_manager")
+    dept_emp_df = read_table("dept_emp")
+    departments_df = read_table("departments")
+
+    # save table names
+    employees_df.createOrReplaceTempView("employees")
+    salaries_df.createOrReplaceTempView("salaries")
+    dept_managers_df.createOrReplaceTempView("dept_manager")
+    dept_emp_df.createOrReplaceTempView("dept_emp")
+    departments_df.createOrReplaceTempView("departments")
 
 
 # 181. Employees Earning More Than Their Managers
